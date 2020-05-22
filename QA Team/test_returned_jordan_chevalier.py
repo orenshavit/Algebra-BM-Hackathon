68298846d5e0c7fda9dfaccddb142615dd5b5d0e
idanfi <idanfi@users.noreply.github.com>
2020-05-22 12:17:38 +0300

    extras

--- QA Team/test_returned_jordan_chevalier.py
+++ QA Team/test_returned_jordan_chevalier.py
@@ -1,22 +1,31 @@
 import numpy as np
-def isDiagnosiable(M):
+def is_diagnosiable(M):
     values, vectors = np.linalg.eig(np.array(M))
     if len(vectors) != len(M):
         print("S not diagoniasble")
         return False
     return True
-def test_returned_jordan_chevallier (T,S,N):
-    shape_S = S.shape
-    shape_N = N.shape
-    shape_T = T.shape
-    if shape_T != shape_S :
+def is_nilpotent(M):
+    values = np.linalg.eigvals(np.array(M))
+    for i in values:
+        if i != 0:
+            return False
+    return True
+def test_returned_jordan_chevallier(T, S, N):
+    shape_s = S.shape
+    shape_n = N.shape
+    shape_t = T.shape
+    if shape_t != shape_s:
         print("Unmatching dimensions of T,S")
         return False
-    if shape_S != shape_N :
+    if shape_s != shape_n:
         print("Unmatching dimensions of S,N")
         return False
-    if shape_T != shape_N:
+    if shape_t != shape_n:
         print("Unmatching dimensions of T,N")
+        return False
+    if shape_s[0] != shape_s[1]:
+        print("T not square matrix")
         return False
     X = np.add(S, N)
     for i in range(len(S)):
@@ -31,11 +40,11 @@ def test_returned_jordan_chevallier (T,S,N):
             if X[i, j] != T[i, j]:
                 print("NS != SN")
                 return False
-    if not isDiagnosiable(S):
+    if not is_diagnosiable(S):
         print("S is not diagonal")
-
+        return False
+    if not is_nilpotent(N):
+        print("N is not Nilpotent")
+        return False
     return True
-T = np.matrix('1 1;0 1')
-S = np.matrix('1 0;0 1')
-N = np.matrix('0 1;0 0')
-test_returned_jordan_chevallier(T, S,  N)
+
