02da6b5e3fd1005a6a9b7202006bf81c5c226e6c
assaf2017 <assafyaffe16@gmail.com>
2020-05-22 11:51:30 +0300

    Fix display

--- print_functions.py
+++ print_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import array_to_latex as a2l
-from IPython.display import Latex
+from IPython.display import Latex, display
 
 
 def print_matrix(np_array_matrix):
@@ -8,11 +8,11 @@ def print_matrix(np_array_matrix):
     Parameters:
     np_array_matrix - 2 dimensional ndarray.
 
-    returns IPython Latex object representing the input matrix.
+    Prints the input matrix.
     """
 
     assert len(
         np_array_matrix.shape) == 2, "Are you rying to print multi dimmensional matrix?"
     latex_code = a2l.to_ltx(
         np_array_matrix, frmt='{:2f}', arraytype='pmatrix', print_out=False)
-    return Latex(latex_code)
+    display(Latex(latex_code))
