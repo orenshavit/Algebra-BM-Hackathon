935a3389470ad4e3eaec7368e60e2fc93081966a
assaf2017 <assafyaffe16@gmail.com>
2020-05-22 11:45:57 +0300

    Add docs

--- print_functions.py
+++ print_functions.py
@@ -4,6 +4,15 @@ from IPython.display import Latex
 
 
 def print_matrix(np_array_matrix):
+    """
+    Parameters:
+    np_array_matrix - 2 dimensional ndarray.
+
+    returns IPython Latex object representing the input matrix.
+    """
+
+    assert len(
+        np_array_matrix.shape) == 2, "Are you rying to print multi dimmensional matrix?"
     latex_code = a2l.to_ltx(
         np_array_matrix, frmt='{:2f}', arraytype='pmatrix', print_out=False)
     return Latex(latex_code)
